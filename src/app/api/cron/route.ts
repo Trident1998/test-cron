86167cd79d17b82386007a1599c9a566712ba32b
Dmytro <dima4994@gmail.com>
2025-06-14 09:19:37 +0300

    first

---
+++ src/app/api/cron/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    return NextResponse.json('Hello world!');
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: 'Error fetching users' },
+      { status: 500 }
+    );
+  }
+}

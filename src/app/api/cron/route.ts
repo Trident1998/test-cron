0bd2db8cc31a2d637d6ab64165305819a7c09bca
Dmytro <dima4994@gmail.com>
2025-06-14 09:32:43 +0300

    added cron

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,8 +1,14 @@
 import { NextResponse } from 'next/server';
 
+let counter = 0;
+
 export async function GET() {
   try {
-    return NextResponse.json('Hello world!');
+    counter += 1;
+    return NextResponse.json({
+      message: 'Hello world!',
+      executionCount: counter,
+    });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
